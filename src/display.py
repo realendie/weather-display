7425a740dc89d06aa31d6f78a20992a23f6d3399
Wyatt Johnson <enderprooffical@gmail.com>
2025-12-17 21:53:23 -0500

    Started working on updated tui

--- src/display.py
+++ src/display.py
@@ -2,10 +2,14 @@ import pytermgui as ptg
 
 with ptg.WindowManager() as manager:
     location = "No Location Specified"
+    tempurature = 94
 
     window = (
         ptg.Window(
-            ptg.Label(f"[bold]{location}")
+            ptg.Splitter(
+                ptg.Label(f"[bold]{location}", parent_align=0),
+                ptg.Label(f"[bold]Tempurature: {tempurature}"),
+            )
         )
         .set_title("weather-display")
         .center()
