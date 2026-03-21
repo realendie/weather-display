ebd2887f228f7bf74ea8e31139c2576c97baca05
Wyatt Johnson <enderprooffical@gmail.com>
2026-03-21 12:23:58 -0400

    Added submit button (No functionality)

--- src/location.py
+++ src/location.py
@@ -7,15 +7,21 @@ with ptg.WindowManager() as manager:
     state_field = ptg.InputField(prompt="State: "),
     country_field = ptg.InputField(prompt="Country: "),
 
+    def submit_info(button):
+        print('Submited')
+
     location_fields = ptg.Container(
         city_field,
         state_field,
         country_field,
     )
 
+    submit_button = ptg.Button("Submit", onclick=submit_info)
+
     window = (
         ptg.Window(
             location_fields,
+            submit_button,
             width=0,
         )
         .set_title("Select Location")
