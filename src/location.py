6b492c73c3ab47d65c49ded05156e5f38714aa1e
Wyatt Johnson <enderprooffical@gmail.com>
2026-03-21 12:57:39 -0400

    Made location.py a function that can be ran from another file and that would return the location field output

--- src/location.py
+++ src/location.py
@@ -1,30 +1,42 @@
 import pytermgui as ptg
 
-with ptg.WindowManager() as manager:
-    manager.layout.add_slot("body")
+def get_location():
+    result = ("", "", "")
 
-    city_field = ptg.InputField(prompt="City: ")
-    state_field = ptg.InputField(prompt="State: ")
-    country_field = ptg.InputField(prompt="Country: ")
-
-    def submit_info(button):
-        print('Submited')
-
-    location_fields = ptg.Container(
-        city_field,
-        state_field,
-        country_field,
-    )
-
-    submit_button = ptg.Button("Submit", onclick=submit_info)
-
-    window = (
-        ptg.Window(
-            location_fields,
-            submit_button,
-            width=0,
+    def submit_info(*_, button):
+        nonlocal result
+        result = (
+            city = city_field.value
+            state = state_field.value
+            country = country_field.value
         )
-        .set_title("Select Location")
-    )
+        manager.stop()
 
-    manager.add(window)
+    with ptg.WindowManager() as manager:
+        manager.layout.add_slot("body")
+
+        city_field = ptg.InputField(prompt="City: ")
+        state_field = ptg.InputField(prompt="State: ")
+        country_field = ptg.InputField(prompt="Country: ")
+
+
+        location_fields = ptg.Container(
+            city_field,
+            state_field,
+            country_field,
+        )
+
+        submit_button = ptg.Button("Submit", onclick=submit_info)
+
+        window = (
+            ptg.Window(
+                location_fields,
+                submit_button,
+                width=0,
+            )
+            .set_title("Select Location")
+        )
+
+        manager.add(window)
+    
+    return result
